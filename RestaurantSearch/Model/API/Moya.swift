1f9bd21e6af28158d620b3c3e13fb579100a3489
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-06 20:37:58 +0900

    Moyaを用いてRestaurantAPIのターゲットを設定

--- RestaurantSearch/Model/API/Moya.swift
+++ RestaurantSearch/Model/API/Moya.swift
@@ -5,4 +5,63 @@
 //  Created by Toshiyana on 2022/07/05.
 //
 
-import Foundation
+import Moya
+
+enum RestaurantAPI {
+    case search(keyValue: [String: Any])
+}
+
+extension RestaurantAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: "https://webservice.recruit.co.jp/")!
+    }
+
+    var path: String {
+        switch self {
+        case .search:
+            return "hotpepper/gourmet/v1/"
+        }
+    }
+
+    var method: Method {
+        switch self {
+        case .search:
+            return .get
+        }
+    }
+
+    var parameters: [String: Any] {
+        var parameter = [
+            "key": APIKey.hotpepperApiKey,
+            "format": "json",
+            "count": 50
+        ] as [String: Any]
+
+        switch self {
+        case .search(let keyValue):
+            keyValue.forEach({key, value in
+                parameter[key] = value
+            })
+            return parameter
+        }
+    }
+
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        case .search:
+            return Moya.URLEncoding.queryString
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .search:
+            print("DEBUG: \(parameters)")
+            return .requestParameters(parameters: parameters, encoding: parameterEncoding)
+        }
+    }
+
+    var headers: [String: String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
