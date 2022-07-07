6864f5dc7fc9743d196b6de08853df657e707a53
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-07 20:30:01 +0900

    API通信のRepositoryを追加

--- RestaurantSearch/Model/API/Repository.swift
+++ RestaurantSearch/Model/API/Repository.swift
@@ -5,4 +5,21 @@
 //  Created by Toshiyana on 2022/07/06.
 //
 
-import Foundation
+// import Foundation
+import Moya
+import RxSwift
+
+final class Repository {
+    private static let apiProvider = MoyaProvider<RestaurantAPI>()
+}
+
+extension Repository {
+    static func search(keyValue: [String: Any]) throws -> Observable<HotPepperResponse> {
+        return apiProvider.rx.request(.search(keyValue: keyValue))
+            .map { response in
+                try APIResponseStatusCodeHandler.handleStatusCode(response)
+                return try JSONDecoder()
+                    .decode(HotPepperResponse.self, from: response.data)
+            }.asObservable()
+    }
+}
