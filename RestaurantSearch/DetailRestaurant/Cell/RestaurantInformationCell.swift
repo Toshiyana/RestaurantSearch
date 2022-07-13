18d494eb7104acd4d149ad5499fd5abb0fa741e1
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-14 03:07:39 +0900

    .swiftlintからblock_based_kvoルールを無効化

---
+++ RestaurantSearch/DetailRestaurant/Cell/RestaurantInformationCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantInformationCell.swift
+//  RestaurantSearch
+//
+//  Created by Toshiyana on 2022/07/13.
+//
+
+import UIKit
+
+class RestaurantInformationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
