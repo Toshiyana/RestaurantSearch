e5b0534324fdb4ec1242abecbeac9a4a13448f96
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-07-15 17:35:37 +0900

    refactor: SearchFilter画面のrangeButtonのsubscribe処理をViewに移した

--- RestaurantSearch/SearchFilter/SearchFilterViewController.swift
+++ RestaurantSearch/SearchFilter/SearchFilterViewController.swift
@@ -10,13 +10,7 @@ import RxSwift
 import RxCocoa
 
 final class SearchFilterViewController: UIViewController {
-    private lazy var viewModel = SearchFilterViewModel(
-        rangeFirstButtonTapped: rangeFirstButton.rx.tap.asObservable(),
-        rangeSecondButtonTapped: rangeSecondButton.rx.tap.asObservable(),
-        rangeThirdButtonTapped: rangeThirdButton.rx.tap.asObservable(),
-        rangeFourthButtonTapped: rangeFourthButton.rx.tap.asObservable(),
-        rangeFifthButtonTapped: rangeFifthButton.rx.tap.asObservable()
-    )
+    private lazy var viewModel = SearchFilterViewModel()
     private let disposeBag = DisposeBag()
 
     @IBOutlet private weak var closeBarButton: UIBarButtonItem!
@@ -27,6 +21,12 @@ final class SearchFilterViewController: UIViewController {
     @IBOutlet private weak var rangeThirdButton: UIButton! // ~1000m
     @IBOutlet private weak var rangeFourthButton: UIButton! // ~2000m
     @IBOutlet private weak var rangeFifthButton: UIButton! // ~3000m
+
+    @IBOutlet private weak var japaneseSakeButton: UIButton!
+    @IBOutlet private weak var cocktailButton: UIButton!
+    @IBOutlet private weak var shochuButton: UIButton!
+    @IBOutlet private weak var wineButton: UIButton!
+    @IBOutlet private weak var freeDrinkButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +40,12 @@ final class SearchFilterViewController: UIViewController {
         rangeThirdButton.layer.cornerRadius = rangeThirdButton.frame.height / 2
         rangeFourthButton.layer.cornerRadius = rangeFourthButton.frame.height / 2
         rangeFifthButton.layer.cornerRadius = rangeFifthButton.frame.height / 2
+
+        japaneseSakeButton.layer.cornerRadius = japaneseSakeButton.frame.height / 2
+        cocktailButton.layer.cornerRadius = cocktailButton.frame.height / 2
+        shochuButton.layer.cornerRadius = shochuButton.frame.height / 2
+        wineButton.layer.cornerRadius = wineButton.frame.height / 2
+        freeDrinkButton.layer.cornerRadius = freeDrinkButton.frame.height / 2
     }
 
     private func bindViewModel() {
@@ -47,6 +53,43 @@ final class SearchFilterViewController: UIViewController {
             .subscribe { [weak self] _ in
                 guard let strongSelf = self else { return }
                 strongSelf.dismiss(animated: true)
+            }
+            .disposed(by: disposeBag)
+
+        // MARK: - Range
+        // 1 - ~300m
+        rangeFirstButton.rx.tap
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.rangeSelected.onNext(1)
+            }
+            .disposed(by: disposeBag)
+        // 2 - ~500m
+        rangeSecondButton.rx.tap
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.rangeSelected.onNext(2)
+            }
+            .disposed(by: disposeBag)
+        // 3 - ~1000m
+        rangeThirdButton.rx.tap
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.rangeSelected.onNext(3)
+            }
+            .disposed(by: disposeBag)
+        // 4 - ~2000m
+        rangeFourthButton.rx.tap
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.rangeSelected.onNext(4)
+            }
+            .disposed(by: disposeBag)
+        // 5 - ~3000m
+        rangeFifthButton.rx.tap
+            .subscribe { [weak self] _ in
+                guard let strongSelf = self else { return }
+                strongSelf.viewModel.rangeSelected.onNext(5)
             }
             .disposed(by: disposeBag)
 
@@ -71,7 +114,7 @@ final class SearchFilterViewController: UIViewController {
         viewModel.getFilterSetting.onNext(Void())
     }
 
-    func activeRange(button: UIButton, active: Bool) {
+    private func activeRange(button: UIButton, active: Bool) {
         if active {
             button.backgroundColor = .systemIndigo
             button.setTitleColor(.white, for: .normal)
--- RestaurantSearch/SearchFilter/SearchFilterViewModel.swift
+++ RestaurantSearch/SearchFilter/SearchFilterViewModel.swift
@@ -16,50 +16,7 @@ final class SearchFilterViewModel {
     let activeRange = PublishSubject<Int>()
     let getFilterSetting = PublishSubject<Void>()
 
-    init(
-        rangeFirstButtonTapped: Observable<Void>,
-        rangeSecondButtonTapped: Observable<Void>,
-        rangeThirdButtonTapped: Observable<Void>,
-        rangeFourthButtonTapped: Observable<Void>,
-        rangeFifthButtonTapped: Observable<Void>
-    ) {
-        // MARK: - Range
-        // 1 - ~300m
-        rangeFirstButtonTapped
-            .subscribe { [weak self] _ in
-                guard let strongSelf = self else { return }
-                strongSelf.rangeSelected.onNext(1)
-            }
-            .disposed(by: disposeBag)
-        // 2 - ~500m
-        rangeSecondButtonTapped
-            .subscribe { [weak self] _ in
-                guard let strongSelf = self else { return }
-                strongSelf.rangeSelected.onNext(2)
-            }
-            .disposed(by: disposeBag)
-        // 3 - ~1000m
-        rangeThirdButtonTapped
-            .subscribe { [weak self] _ in
-                guard let strongSelf = self else { return }
-                strongSelf.rangeSelected.onNext(3)
-            }
-            .disposed(by: disposeBag)
-        // 4 - ~2000m
-        rangeFourthButtonTapped
-            .subscribe { [weak self] _ in
-                guard let strongSelf = self else { return }
-                strongSelf.rangeSelected.onNext(4)
-            }
-            .disposed(by: disposeBag)
-        // 5 - ~3000m
-        rangeFifthButtonTapped
-            .subscribe { [weak self] _ in
-                guard let strongSelf = self else { return }
-                strongSelf.rangeSelected.onNext(5)
-            }
-            .disposed(by: disposeBag)
-
+    init() {
         // Range: 1-300m ~ 5-3000m
         rangeSelected
             .subscribe { [weak self] rangeNumber in
